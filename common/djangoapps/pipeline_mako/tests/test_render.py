985b241bb435a80648b30f300bc4085dcc3ed155
Andy Armstrong <andya@edx.org>
2015-06-29 14:27:19 -0400

    Use the correct MD5-hashed version of files

--- common/djangoapps/pipeline_mako/tests/test_render.py
+++ common/djangoapps/pipeline_mako/tests/test_render.py
@@ -1,29 +1,12 @@
 """Tests for rendering functions in the mako pipeline. """
 
 from django.test import TestCase
-from pipeline_mako import render_require_js_path_overrides
+from pipeline_mako import require_js_path
 
 
-class RequireJSPathOverridesTest(TestCase):
-    """Test RequireJS path overrides. """
+class RequireJSPathTest(TestCase):
+    """Test RequireJS path handling. """
 
-    OVERRIDES = {
-        'jquery': 'js/vendor/jquery.min.js',
-        'backbone': 'js/vendor/backbone-min.js',
-        'text': 'js/vendor/text.js'
-    }
-
-    OVERRIDES_JS = (
-        "<script type=\"text/javascript\">\n"
-        "var require = require || {};\n"
-        "require.paths = require.paths || [];\n"
-        "require.baseUrl = '/static/'\n"
-        "require.paths['jquery'] = 'js/vendor/jquery.min';\n"
-        "require.paths['text'] = 'js/vendor/text';\n"
-        "require.paths['backbone'] = 'js/vendor/backbone-min';\n"
-        "</script>"
-    )
-
-    def test_requirejs_path_overrides(self):
-        result = render_require_js_path_overrides(self.OVERRIDES)
-        self.assertEqual(result, self.OVERRIDES_JS)
+    def test_require_js_path(self):
+        result = require_js_path('js/vendor/jquery.min.js')
+        self.assertEqual(result, 'js/vendor/jquery.min')
